245692343c70c7271ad5adaa496c730fdf8f6b7a
Christian Kropfberger <christian@kropfberger.at>
2021-07-26 11:20:29 +0200

    This is a very simply initial version.

--- Sources/UniqueIDProvider/UniqueIDProvider.swift
+++ Sources/UniqueIDProvider/UniqueIDProvider.swift
@@ -1,6 +1,18 @@
-public struct UniqueIDProvider {
-    public private(set) var text = "Hello, World!"
+public class UniqueIDProvider {
+    private static let handle: UniqueIDProvider = UniqueIDProvider()
 
-    public init() {
+    public static func sharedInstance() -> UniqueIDProvider {
+        return handle
+    }
+    
+    private var counter: Int
+    
+    public func next() -> Int {
+        counter += 1
+        return counter
+    }
+    
+    private init() {
+        counter = 0
     }
 }
--- Tests/UniqueIDProviderTests/UniqueIDProviderTests.swift
+++ Tests/UniqueIDProviderTests/UniqueIDProviderTests.swift
@@ -2,10 +2,35 @@ import XCTest
 @testable import UniqueIDProvider
 
 final class UniqueIDProviderTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(UniqueIDProvider().text, "Hello, World!")
+    func testSingletonHandle() throws {
+        let idHandle = UniqueIDProvider.sharedInstance()
+        XCTAssertNotNil(idHandle, "idHandle should not be nil but it is.")
+    }
+    
+    func testSingletonUniqueHandle() throws {
+        let idHandle1 = UniqueIDProvider.sharedInstance()
+        XCTAssertNotNil(idHandle1, "idHandle1 should not be nil but it is.")
+        
+        let idHandle2 = UniqueIDProvider.sharedInstance()
+        XCTAssertNotNil(idHandle2, "idHandle2 should not be nil but it is.")
+    }
+    
+    func testValues() throws {
+        let id = UniqueIDProvider.sharedInstance()
+        
+        let v1 = id.next()
+        let v2 = id.next()
+        
+        XCTAssertNotEqual(v1, v2)
+    }
+    
+    func testMoreValues() throws {
+        let id1 = UniqueIDProvider.sharedInstance()
+        let v1 = id1.next()
+
+        let id2 = UniqueIDProvider.sharedInstance()
+        let v2 = id2.next()
+        
+        XCTAssertNotEqual(v1, v2)
     }
 }
